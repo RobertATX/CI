f61065e424bf5ae004f53b3fb73fe8c92356acb9
Robert Donovan <admin@mixfin.com>
2015-11-26 21:28:17 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,8 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
-sudo apt-get install -y fail2ban python-pip python-dev python-virtualenv
+sudo apt-get install -y fail2ban python-pip python-dev python-virtualenv git
 sudo pip install ansible
 sudo mkdir /etc/ansible
 sudo touch /etc/ansible/hosts
+git clone https://github.com/RobertATX/CI.git
