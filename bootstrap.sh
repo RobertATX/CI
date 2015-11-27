adfcd27102d831db94debde82ae76ceb3bd729df
Robert Donovan <admin@mixfin.com>
2015-11-26 21:21:25 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Installing Ansible..."
-#apt-get install -y software-properties-common
-#apt-add-repository ppa:ansible/ansible
-#apt-get update
-#apt-get install -y ansible
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
 apt-get install -y fail2ban python-pip python-dev python-virtualenv
 pip install ansible
 mkdir /etc/ansible
