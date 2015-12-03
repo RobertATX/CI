fdd7e0b65b9c866fb423f5f334141a52e8436adf
Robert Donovan <admin@mixfin.com>
2015-12-03 08:48:55 -0600

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ansible-playbook ./CI/prod/deploy.yml --private-key=\
-./ssh_keys/prod_key_np -u deployer -i ./CI/prod/hosts
+#ansible-playbook ./CI/prod/deploy.yml --private-key=\
+#./ssh_keys/prod_key_np -u deployer -i ./CI/prod/hosts
+ansible-playbook -c local deploy.yml 
